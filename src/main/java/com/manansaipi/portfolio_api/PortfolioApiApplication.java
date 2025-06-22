65a8ff4707a89dd7853c44e338c42dff9505e009
manansaipi <boben.manan5@gmail.com>
2025-06-22 22:30:39 +0700

    add .env

--- src/main/java/com/manansaipi/portfolio_api/PortfolioApiApplication.java
+++ src/main/java/com/manansaipi/portfolio_api/PortfolioApiApplication.java
@@ -3,11 +3,19 @@ package com.manansaipi.portfolio_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class PortfolioApiApplication {
 
 	public static void main(String[] args) {
+  		Dotenv dotenv = Dotenv.configure().load();
+
+        // Optional: Set them as system properties
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		
 		SpringApplication.run(PortfolioApiApplication.class, args);
 	}
-
 }
